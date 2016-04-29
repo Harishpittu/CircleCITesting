e8c7bf1bd1bc856b99d592d62c4776311cd176df
Harish <harish@7cstudio.com>
2016-04-29 14:06:56 +0530

    more changes dd

--- CircleCi/app/src/main/java/com/sevencstudio/harish/myapplication/MainActivity.java
+++ CircleCi/app/src/main/java/com/sevencstudio/harish/myapplication/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
     }
     public void showToast(View v)
     {
-        Toast.makeText(MainActivity.this, "testing CI kjjhhjbjd", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "testing CI more", Toast.LENGTH_SHORT).show();
     }
 }
